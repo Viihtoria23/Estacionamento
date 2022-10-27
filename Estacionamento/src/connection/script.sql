c4e1ea5808fac697e25abc46e1c7149ffab4b1e7
04801167098 <04801167098@LAB205-29.fw.iffar.edu.br>
2022-10-27 10:08:49 -0600

    semana 5 - cadastro motorista

--- Estacionamento/src/connection/script.sql
+++ Estacionamento/src/connection/script.sql
@@ -17,3 +17,16 @@ numero  int NOT NULL,
 rua     varchar(100) NOT NULL,
 obliqua boolean NOT NULL,
 PRIMARY KEY (idVaga));
+
+CREATE TABLE motorista(
+idMotorista  int NOT NULL AUTO_INCREMENT,
+nomeCompleto  varchar(77) NOT NULL,
+genero    varchar(22) NOT NULL,
+rg  varchar(9) NOT NULL,
+cpf  varchar(10) NOT NULL,
+celular  varchar(9) NOT NULL,
+email  varchar(33) NOT NULL,
+senha  varchar(8) NOT NULL,
+PRIMARY KEY (idMotorista));
+
+
--- Estacionamento/src/view/JFCadastrarVaga.java
+++ Estacionamento/src/view/JFCadastrarVaga.java
@@ -46,8 +46,6 @@ public class JFCadastrarVaga extends javax.swing.JFrame {
 
         label1.setText("label1");
 
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 14)); // NOI18N
         jLabel1.setText("Cadastrar Vaga");
 
